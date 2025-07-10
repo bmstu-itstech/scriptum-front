247ecf71cf251203b48a8f7131b29f188d24eea9
Robocotik <alexander.starkin@gmail.com>
2025-07-10 22:23:08 +0300

    [feat] pages/ main: add main page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {/* <Header/> */}
         {children}
+        {/* <Footer/> */}
       </body>
     </html>
   );
---
+++ src/app/page.usecase.tsx
@@ -0,0 +1,10 @@
+// interface IMainPage {
+//     title: string;
+//     subtitle: string;
+//     children :
+// }
+
+export const mainPageUsecase = {
+  title: 'Управление скриптами',
+  subtitle: 'Найдите и запустите нужные скрипты или создайте новые',
+};
