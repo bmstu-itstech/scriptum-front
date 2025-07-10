e168784d92030b3178b4fe583a994a0daddc6c8e
Robocotik <alexander.starkin@gmail.com>
2025-07-10 22:22:03 +0300

    [feat] components: pagelayout: add page layout

--- src/shared/Container/index.tsx
+++ src/shared/Container/index.tsx
@@ -1,7 +1,12 @@
 import type {Props} from '@/shared/Container/Container.props';
 import type {FC} from 'react';
 import styles from '@/shared/Container/Container.module.css';
+import cn from 'classnames';
 
-export const Container: FC<Props> = ({children}) => {
-  return <div className={styles.container}>{children}</div>;
+export const Container: FC<Props> = ({children, className, ...props}) => {
+  return (
+    <div className={cn(styles.container, className)} {...props}>
+      {children}
+    </div>
+  );
 };
