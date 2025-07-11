a2b17ae20ebb2d62a3d638ffb2dbd70450699717
Robocotik <alexander.starkin@gmail.com>
2025-07-11 23:07:13 +0300

    [feat] components: add ScriptInfo

---
+++ src/components/ScriptInfo/index.tsx
@@ -0,0 +1,25 @@
+import { Props } from '@/components/ScriptInfo/ScriptInfo.props';
+import type {FC} from 'react';
+import cn from 'classnames';
+import {getDate} from '@/utils/getRowFromDate';
+import styles from '@/components/ScriptInfo/ScriptInfo.module.css';
+
+export const ScriptInfo: FC<Props> = ({
+  scriptTitle,
+  author,
+  data,
+  subtitle,
+  className,
+  ...props
+}) => {
+  return (
+    <div className={cn(className, styles.ScriptInfo)} {...props}>
+      <h1 className={cn(styles.ScriptInfo__title, 'layout__title')}>{scriptTitle}</h1>
+      <h2 className={cn(styles.ScriptInfo__subtitle, 'layout__subtitle')}>{subtitle}</h2>
+      <div className={styles.ScriptInfo__info}>
+        <p className={styles.ScriptInfo__author}>Автор: {author}</p>
+        <p className={styles.ScriptInfo__date}>Создан {getDate(data)}</p>
+      </div>
+    </div>
+  );
+};
