203254e310fbd3ab3a9a89739726800297806e79
Robocotik <alexander.starkin@gmail.com>
2025-07-11 09:20:51 +0300

    [feat] icons: add calendar

---
+++ src/components/icons/CalendarIcon/index.tsx
@@ -0,0 +1,8 @@
+import Image from 'next/image';
+import src from '@/assets/icons/calendar.svg';
+import type {FC} from 'react';
+import { Props } from '@/components/icons/CalendarIcon/CalendarIcon.props';
+
+export const CalendarIcon: FC<Props> = ({className, ...props}) => {
+  return <Image alt='CalendarIcon' src={src} className={className} {...props} />;
+};
