7d87f9c8f0dcee8c1f79946be53630d974144e46
Robocotik <alexander.starkin@gmail.com>
2025-07-11 10:42:40 +0300

    [feat] icons: add AddIcon

---
+++ src/components/icons/Addicon/index.tsx
@@ -0,0 +1,7 @@
+import Image from 'next/image';
+import src from '@/assets/icons/addIcon.svg';
+import type {FC} from 'react';
+import { Props } from '@/components/icons/Addicon/AddIcon.props';
+export const AddIcon: FC<Props> = ({className, ...props}) => {
+  return <Image alt='AddIcon' src={src} className={className} {...props} />;
+};
