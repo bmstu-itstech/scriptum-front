11096cd092dcd3445f9fbe9f79f211f957380187
Shahmar <abdull.shah@mail.ru>
2025-07-13 00:06:28 +0300

    [feat] header: correct bnt in carts

--- src/components/ScriptsPanel/components/ScriptElement/index.tsx
+++ src/components/ScriptsPanel/components/ScriptElement/index.tsx
@@ -1,47 +1,48 @@
-import type {Props} from '@/components/ScriptsPanel/components/ScriptElement/ScriptElement.props';
-import type {FC} from 'react';
+import type { Props } from '@/components/ScriptsPanel/components/ScriptElement/ScriptElement.props';
+import type { FC } from 'react';
 import cn from 'classnames';
 import styles from '@/components/ScriptsPanel/components/ScriptElement/ScriptElement.module.css';
 import basicStyles from '@/components/ScriptsPanel/components/EmptyScript/EmptyScript.module.css';
-import {Button} from '@/shared/Button';
-import {RunIcon} from '@/components/icons/RunIcon';
-import {getDate} from '@/utils/getRowFromDate';
-import {TextWithIcon} from '@/shared/TextWithIcon';
-import {PersonIcon} from '@/components/icons/PersonIcon';
-import {CalendarIcon} from '@/components/icons/CalendarIcon';
+import { Button } from '@/layouts/Button';
+import { RunIcon } from '@/components/icons/RunIcon';
+import { getDate } from '@/utils/getRowFromDate';
+import { TextWithIcon } from '@/shared/TextWithIcon';
+import { PersonIcon } from '@/components/icons/PersonIcon';
+import { CalendarIcon } from '@/components/icons/CalendarIcon';
 
 export const ScriptElement: FC<Props> = ({
-  className,
-  title,
-  id,
-  countOfRuns,
-  subtitle,
-  author,
-  data,
-  ...props
+	className,
+	title,
+	id,
+	countOfRuns,
+	subtitle,
+	author,
+	data,
+	...props
 }) => {
-  return (
-    <div
-      id={id.toString()}
-      className={cn(className, styles.scriptElement, basicStyles.layout)}
-      {...props}>
-      <div className={styles.scriptElement__supblock}>
-        <h2 className={styles.scriptElement__title}>{title}</h2>
-        <p className={styles.scriptElement__runs}>Кол. запусков: {countOfRuns}</p>
-      </div>
+	return (
+		<div
+			id={id.toString()}
+			className={cn(className, styles.scriptElement, basicStyles.layout)}
+			{...props}>
+			<div className={styles.scriptElement__supblock}>
+				<h2 className={styles.scriptElement__title}>{title}</h2>
+				<p className={styles.scriptElement__runs}>Кол. запусков: {countOfRuns}</p>
+			</div>
 
-      <h3 className={styles.scriptElement__subtitle}>{subtitle}</h3>
-      <div className={styles.scriptElement__info}>
-        <TextWithIcon icon={<PersonIcon />} className={styles.scriptElement__author}>
-          {author}
-        </TextWithIcon>
-        <TextWithIcon icon={<CalendarIcon />} className={styles.scriptElement__data}>
-          {getDate(data)}
-        </TextWithIcon>
-      </div>
-      <div className={styles.scriptElement__interactive}>
-        <Button icon={<RunIcon />}>Запустить</Button>
-      </div>
-    </div>
-  );
+			<h3 className={styles.scriptElement__subtitle}>{subtitle}</h3>
+			<div className={styles.scriptElement__info}>
+				<TextWithIcon icon={<PersonIcon />} className={styles.scriptElement__author}>
+					{author}
+				</TextWithIcon>
+				<TextWithIcon icon={<CalendarIcon />} className={styles.scriptElement__data}>
+					{getDate(data)}
+				</TextWithIcon>
+			</div>
+			<div className={styles.scriptElement__interactive}>
+				<Button className='openScriptBtn' icon={<RunIcon />}>Запустить</Button>
+			</div>
+
+		</div>
+	);
 };
