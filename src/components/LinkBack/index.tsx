fff6f185840469368659da8778250113a4c7a1b8
Shahmar <abdull.shah@mail.ru>
2025-07-20 18:30:15 +0300

    [feat] feature: add adaptive in run script

--- src/components/LinkBack/index.tsx
+++ src/components/LinkBack/index.tsx
@@ -3,14 +3,15 @@ import type { FC } from 'react';
 import { Props } from '@/components/LinkBack/LinkBack.props';
 import { useRouter } from 'next/navigation';
 import style from '@/components/LinkBack/LinkBack.module.css';
+import cn from 'classnames';
 
 
 export const LinkBack: FC<Props> = ({ title, icon, className, ...props }) => {
 	const router = useRouter();
 	return (
-		<div className={style.LinkBack} onClick={() => router.back()} {...props}>
+		<div className={cn(style.LinkBack)} onClick={() => router.back()} {...props}>
 			{icon}
-			<p className={className}>{title}</p>
+			<p className={cn(className)}>{title}</p>
 		</div>
 	);
 };
--- src/components/ScriptParametrs/ScriptParametrs.usecase.tsx
+++ src/components/ScriptParametrs/ScriptParametrs.usecase.tsx
@@ -1,27 +1,28 @@
 import cn from 'classnames';
 import styles from '@/components/ScriptParametrs/ScriptParametrs.module.css';
-import {GalkaIcon} from '@/components/icons/GalkaIcon';
-import {AttentionIcon} from '@/components/icons/AttentionIcon';
+import { GalkaIcon } from '@/components/icons/GalkaIcon';
+import { AttentionIcon } from '@/components/icons/AttentionIcon';
+
 export const ScriptParametersInputUsecase = {
-  header: (
-    <div className={styles.ScriptParametrs__header}>
-      <h2 className={styles.ScriptParametrs__title}>
-        <AttentionIcon />
-        <p className='layout__title-sm'>Входные параметры</p>
-      </h2>
-      <h4 className='layout__subtitle'>Заполните необходимые параметры для запуска скрипта</h4>
-    </div>
-  ),
+	header: (
+		<div className={styles.ScriptParametrs__header}>
+			<h2 className={styles.ScriptParametrs__title}>
+				<AttentionIcon />
+				<p className='layout__title-sm'>Входные параметры</p>
+			</h2>
+			<h4 className='layout__subtitle'>Заполните необходимые параметры для запуска скрипта</h4>
+		</div>
+	),
 };
 
 export const ScriptParametersOutputUsecase = {
-  header: (
-    <div className={styles.ScriptParametrs__header}>
-      <h2 className={styles.ScriptParametrs__title}>
-        <GalkaIcon />
-        <p className='layout__title-sm'>Выходные параметры</p>
-      </h2>
-      <h4 className='layout__subtitle'>Скрипт вернет следующие результаты после выполнения</h4>
-    </div>
-  ),
+	header: (
+		<div className={styles.ScriptParametrs__header}>
+			<h2 className={styles.ScriptParametrs__title}>
+				<GalkaIcon />
+				<p className='layout__title-sm'>Выходные параметры</p>
+			</h2>
+			<h4 className='layout__subtitle'>Скрипт вернет следующие результаты после выполнения</h4>
+		</div>
+	),
 };
