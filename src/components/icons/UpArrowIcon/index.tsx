337cef9b484620192b01f33d45c69f92c3f04aa9
Robocotik <alexander.starkin@gmail.com>
2025-07-25 20:48:40 +0300

    [fix] fix react errors connected with naming

--- src/components/icons/UpArrowIcon/index.tsx
+++ src/components/icons/UpArrowIcon/index.tsx
@@ -13,8 +13,8 @@ export const UpArrowIcon: FC<Props> = ({className, ...props}) => {
       {...props}
       viewBox='0 0 512 512'
       xmlSpace='preserve'>
-      <g id='SVGRepo_bgCarrier' stroke-width='0'></g>
-      <g id='SVGRepo_tracerCarrier' stroke-linecap='round' stroke-linejoin='round'></g>
+      <g id='SVGRepo_bgCarrier' strokeWidth='0'></g>
+      <g id='SVGRepo_tracerCarrier' strokeLinecap='round' strokeLinejoin='round'></g>
       <g id='SVGRepo_iconCarrier'>
         {' '}
         <g>
