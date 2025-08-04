b6dca9732bce12dbc9dfb7f7828d35923c7f0e4d
Robocotik <alexander.starkin@gmail.com>
2025-08-05 02:52:13 +0300

    [feat] components: add ExtendedBlock

---
+++ src/shared/ExtendedBlock/ExtendedBlock.props.ts
@@ -0,0 +1,7 @@
+import type { HTMLAttributes, ReactElement } from 'react';
+
+export type Props = HTMLAttributes<HTMLDivElement> & {
+  children: ReactElement[];
+  contentClassname?: string;
+  mainExtendedClassname?: string;
+};
