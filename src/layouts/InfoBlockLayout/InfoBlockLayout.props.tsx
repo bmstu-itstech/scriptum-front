1f19feb26bfd537c77a138132923258f74585a12
Robocotik <alexander.starkin@gmail.com>
2025-08-05 02:50:41 +0300

    [feat] components: add infoBlock

---
+++ src/layouts/InfoBlockLayout/InfoBlockLayout.props.tsx
@@ -0,0 +1,8 @@
+import type {HTMLAttributes, ReactElement} from 'react';
+
+export interface Props extends HTMLAttributes<HTMLDivElement> {
+  header: ReactElement;
+  children: ReactElement[];
+  headerClassname?: string;
+  contentClassname?: string;
+}
