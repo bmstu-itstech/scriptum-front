dfae6fe2c43e776ec2a0765ee2d5a1c7dab31556
Robocotik <alexander.starkin@gmail.com>
2025-08-05 02:50:41 +0300

    [feat] components: add infoBlock

---
+++ src/layouts/InfoBlockLayout/index.tsx
@@ -0,0 +1,14 @@
+'use client';
+import {Props} from '@/layouts/InfoBlockLayout/InfoBlockLayout.props';
+import {type FC} from 'react';
+import cn from 'classnames';
+import styles from '@/layouts/InfoBlockLayout/InfoBlockLayout.module.css';
+
+export const InfoBlockLayout: FC<Props> = ({header, className, headerClassname, contentClassname, children, ...props}) => {
+  return (
+    <div className={cn(className, styles.ScriptParametrsLayout)} {...props}>
+      <div className={cn(styles.ScriptParametrsLayout__header, headerClassname)}>{header}</div>
+      <div className={cn(styles.ScriptParametrsLayout__content, contentClassname)}>{children}</div>
+    </div>
+  );
+};
