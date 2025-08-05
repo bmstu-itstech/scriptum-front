f6843abdfde6db553fd0952942964cf0abbc493b
Robocotik <alexander.starkin@gmail.com>
2025-08-05 17:49:01 +0300

    [choire] conponents: loader

--- src/components/ScriptParametrsLoader/index.tsx
+++ src/components/ScriptParametrsLoader/index.tsx
@@ -1,25 +1,54 @@
 'use client';
-import {ScriptParametrsLayout} from '@/layouts/ScriptParametrsLayout';
-import {useCallback, useState, type FC} from 'react';
-import {Props} from '@/components/ScriptParametrsLoader/ScriptParametrsLoader.props';
-import {pageCreateUsecase} from '@/app/(withHeader)/script/create/page.usecase';
+import { ScriptParametrsLayout } from '@/layouts/ScriptParametrsLayout';
+import { useCallback, useState, type FC } from 'react';
+import { Props } from '@/components/ScriptParametrsLoader/ScriptParametrsLoader.props';
+import { pageCreateUsecase } from '@/app/(withHeader)/script/create/page.usecase';
 import cn from 'classnames';
 import stylesLayout from '@/layouts/ScriptParametrsLayout/ScriptParametrsLayout.module.css';
 import styles from '@/components/ScriptParametrsLoader/ScriptParametersLoader.module.css';
-import {ScriptParametersLoaderRowProps} from '@/components/ScriptParametrsLoader/components/ScriptParametrsLoaderRow/ScriptParametersLoaderRow.props';
-import {Button} from '@/layouts/Button';
-import {AddParametrIcon} from '@/components/icons/AddParametricon';
-import {ScriptParametersLoaderRow} from '@/components/ScriptParametrsLoader/components/ScriptParametrsLoaderRow/index';
+import { ScriptParametersLoaderRowProps } from '@/components/ScriptParametrsLoader/components/ScriptParametrsLoaderRow/ScriptParametersLoaderRow.props';
+import { Button } from '@/layouts/Button';
+import { AddParametrIcon } from '@/components/icons/AddParametricon';
+import { ScriptParametersLoaderRow } from '@/components/ScriptParametrsLoader/components/ScriptParametrsLoaderRow/index';
+import { DocumentNoParamsIcon } from '@/components/icons/DocumentNoParamsIcon';
 
-export const ScriptParametrsLoader: FC<Props> = ({type, className, ...props}) => {
+export const ScriptParametrsLoader: FC<Props> = ({ type, className, ...props }) => {
   const [params, setParams] = useState<ScriptParametersLoaderRowProps[]>([]);
 
   const onClick = useCallback(() => {
-    setParams(prev => [...prev, {name: '', desc: '', measure: '', type: ''}]);
+    setParams(prev => [...prev, { name: '', desc: '', measure: '', type: '' }]);
   }, []);
 
   const [parametrType, setParametrType] = useState('');
   const [parametrMeasure, setParametrMeasure] = useState('');
+
+  const PreBlock = () => {
+    if (params.length === 0) {
+      return (<div className={styles.preblock}>
+        <DocumentNoParamsIcon className={styles.preblock__icon} />
+        <p className={styles.preblock__text}>Параметры не добавлены</p>
+        <p className={styles.preblock__subtext}>Нажмите "Добавить параметр" для начала</p>
+      </div>);
+    }
+    return (
+
+      <div className={cn(styles.headerList, styles.justifyBetween)}>
+        {(type == 'input' ? pageCreateUsecase.input.blocks : pageCreateUsecase.output.blocks).map(
+          (block, ind) => {
+            return (
+              <p key={ind} className={cn(`layout__title-sm`, styles.headerItem)}>
+                {block}
+              </p>
+            );
+          },
+        )}
+      </div>
+    )
+  }
+
+
+
+
   return (
     <ScriptParametrsLayout
       className={className}
@@ -48,19 +77,7 @@ export const ScriptParametrsLoader: FC<Props> = ({type, className, ...props}) =>
           </div>
         )
       }
-      preBlock={
-        <div className={cn(styles.headerList, styles.justifyBetween)}>
-          {(type == 'input' ? pageCreateUsecase.input.blocks : pageCreateUsecase.output.blocks).map(
-            (block, ind) => {
-              return (
-                <p key={ind} className={cn(`layout__title-sm`, styles.headerItem)}>
-                  {block}
-                </p>
-              );
-            },
-          )}
-        </div>
-      }
+      preBlock={<PreBlock />}
       {...props}>
       {params.map((row, ind) => {
         return (
