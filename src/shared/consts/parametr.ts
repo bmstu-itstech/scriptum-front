861d4bdd4ff38442f932e7f08706af60cf7f53fb
Robocotik <alexander.starkin@gmail.com>
2025-08-05 03:02:13 +0300

    [feat] globals + consts

---
+++ src/shared/consts/parametr.ts
@@ -0,0 +1,14 @@
+export enum ParametrMeasure {
+  None = 'Безразмерная',
+  M = 'Метры',
+  CM = 'Сантиметры',
+  MM = 'Миллиметры',
+  T = 'Тонны',
+  KG = 'Килограммы',
+  G = 'Граммы',
+}
+export enum ParametrType {
+  FLOAT = 'Вещественный',
+  INT = 'Целый',
+  COMP = 'Комплексный',
+}
