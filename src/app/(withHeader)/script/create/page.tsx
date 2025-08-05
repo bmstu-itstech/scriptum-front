45f3226e9727d853092a1dc09cdace92b803c745
Robocotik <alexander.starkin@gmail.com>
2025-08-05 17:50:10 +0300

    [feat]  pages: done createPage with small bugs

--- src/app/(withHeader)/script/create/page.tsx
+++ src/app/(withHeader)/script/create/page.tsx
@@ -1,58 +1,73 @@
-import {PageLayout} from '@/layouts/PageLayout';
-import {ScriptParametrsLayout} from '@/layouts/ScriptParametrsLayout';
-import {pageCreateUsecase} from '@/app/(withHeader)/script/create/page.usecase';
-import {InputLayout} from '@/layouts/InputLayout';
-import {InfoBlockLayout} from '@/layouts/InfoBlockLayout';
+'use client'
+import { PageLayout } from '@/layouts/PageLayout';
+import { pageCreateUsecase } from '@/app/(withHeader)/script/create/page.usecase';
+import { InputLayout } from '@/layouts/InputLayout';
+import { InfoBlockLayout } from '@/layouts/InfoBlockLayout';
 import styles from '@/app/(withHeader)/script/create/page.module.css';
-import cn from 'classnames';
-import stylesLayout from '@/layouts/ScriptParametrsLayout/ScriptParametrsLayout.module.css';
-import {ScriptParametrsLoader} from '@/components/ScriptParametrsLoader';
+import { ScriptParametrsLoader } from '@/components/ScriptParametrsLoader';
+import { Formik, Form } from 'formik';
+import { Button } from '@/layouts/Button';
+import { ScriptSchema } from '@/app/(withHeader)/script/create/page.usecase';
+import { SaveScriptIcon } from '@/components/icons/SaveScriptIcon';
 
 export default function CreatePage() {
   return (
     <PageLayout>
-      <InfoBlockLayout
-        headerClassname={styles.infoblock__header}
-        contentClassname={styles.infoblock__content}
-        // mainExtendedClassname={}
-        className={styles.infoblock}
-        header={pageCreateUsecase.main.header}>
-        <div className={styles.flex}>
-          <InputLayout
-            type='text'
-            inputTitle={pageCreateUsecase.main.blocks.scriptTitle.title}
-            placeholder={pageCreateUsecase.main.blocks.scriptTitle.placeholder}
-            errorText={pageCreateUsecase.main.blocks.scriptTitle.errorText}
-          />
-          <InputLayout
-            type='file'
-            inputTitle={pageCreateUsecase.main.blocks.scriptCode.title}
-            placeholder={pageCreateUsecase.main.blocks.scriptCode.placeholder}
-            errorText={pageCreateUsecase.main.blocks.scriptCode.errorText}
-          />
-        </div>
-        <InputLayout
-          type='text'
-          inputClassName={styles.desc}
-          inputTitle={pageCreateUsecase.main.blocks.scriptDesc.title}
-          placeholder={pageCreateUsecase.main.blocks.scriptDesc.placeholder}
-          errorText={pageCreateUsecase.main.blocks.scriptDesc.errorText}
-        />
-      </InfoBlockLayout>
-      <ScriptParametrsLoader
-        type='input'
-        // headerClassname={stylesLayout.smallPadding}
-        // header={pageCreateUsecase.input.header}
-        //   </div>
-        // }
-      />
-      <ScriptParametrsLoader
-        type='output'
-        // headerClassname={stylesLayout.smallPadding}
-        // header={pageCreateUsecase.input.header}
-        //   </div>
-        // }
-      />
+      <Formik
+        initialValues={{ name: '', desc: '', file: null }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+        validationSchema={ScriptSchema}
+      >
+        {({ handleSubmit, handleChange, setFieldValue, values, errors, touched }) => (
+          <Form onSubmit={handleSubmit} className={styles.form}>
+            <InfoBlockLayout
+              headerClassname={styles.infoblock__header}
+              contentClassname={styles.infoblock__content}
+              className={styles.infoblock}
+              header={pageCreateUsecase.main.header}
+            >
+              <div className={styles.flex}>
+                <InputLayout
+                  type='text'
+                  name='name'
+                  value={values.name}
+                  onChange={handleChange}
+                  inputTitle={pageCreateUsecase.main.blocks.scriptTitle.title}
+                  placeholder={pageCreateUsecase.main.blocks.scriptTitle.placeholder}
+                  errorText={errors.name && touched.name ? errors.name : null}
+                />
+                <InputLayout
+                  type='file'
+                  name='file'
+                  value=''
+                  onChange={(event) => {
+                    setFieldValue('file', event.currentTarget.files?.[0]);
+                  }}
+                  inputTitle={pageCreateUsecase.main.blocks.scriptCode.title}
+                  placeholder={pageCreateUsecase.main.blocks.scriptCode.placeholder}
+                  errorText={errors.file && touched.file ? errors.file : null}
+                />
+              </div>
+              <InputLayout
+                type='text'
+                name='desc'
+                value={values.desc}
+                isTextArea
+                onChange={handleChange}
+                inputClassName={styles.desc}
+                inputTitle={pageCreateUsecase.main.blocks.scriptDesc.title}
+                placeholder={pageCreateUsecase.main.blocks.scriptDesc.placeholder}
+                errorText={errors.desc && touched.desc ? errors.desc : null}
+              />
+            </InfoBlockLayout>
+            <ScriptParametrsLoader type='input' />
+            <ScriptParametrsLoader type='output' />
+            <Button className={styles.saveBtn} icon={<SaveScriptIcon />} type='submit'>Сохранить скрипт</Button>
+          </Form>
+        )}
+      </Formik>
     </PageLayout>
   );
 }
