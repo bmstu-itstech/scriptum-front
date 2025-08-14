dc766f630fe856c58e6c3448baa439b730292644
Robocotik <alexander.starkin@gmail.com>
2025-08-14 18:16:44 +0300

    [feat] page: add using of real data

--- src/app/(withHeader)/script/create/page.tsx
+++ src/app/(withHeader)/script/create/page.tsx
@@ -5,26 +5,55 @@ import InputLayout from '@/layouts/InputLayout';
 import { InfoBlockLayout } from '@/layouts/InfoBlockLayout';
 import styles from '@/app/(withHeader)/script/create/page.module.css';
 import { ScriptParametrsLoader } from '@/components/ScriptParametrsLoader';
-import { Formik, Form, FastField, Field } from 'formik';
-import { Button } from '@/layouts/Button';
+import { Formik, Form, FastField, Field, type FastFieldProps } from 'formik';
+import { Button } from '@/shared/Button';
 import { ScriptSchema } from '@/app/(withHeader)/script/create/page.usecase';
 import { SaveScriptIcon } from '@/components/icons/SaveScriptIcon';
+import { useCreateScript } from '@/hooks/script/useCreateScript';
+import { useUploadFile } from '@/hooks/script/useUploadFile';
 
 
 export default function CreatePage() {
 
   // console.log('страница перерендерилпсь')
+  const { mutate: createScript } = useCreateScript()
+  const { mutateAsync: uploadFile } = useUploadFile()
   return (
     <PageLayout>
       <Formik
         initialValues={ScriptInitialValues}
-        onSubmit={(values) => {
-          console.log(values);
-          alert('Я ОТПРАВИЛ')
+        onSubmit={async (values, { setSubmitting }) => {
+          if (values.file) {
+            setSubmitting(true);
+            try {
+              const { file_id } = await uploadFile(values.file);
+              createScript({
+                script_name: values.name,
+                script_description: values.desc,
+                file_id: file_id,
+                in_fields: values.inputParams.map((param) => ({
+                  name: param.name,
+                  description: param.desc,
+                  unit: param.measure,
+                  type: param.type,
+                })),
+                out_fields: values.outputParams.map((param) => ({
+                  name: param.name,
+                  description: param.desc,
+                  unit: param.measure,
+                  type: param.type,
+                }))
+              });
+            } catch (error) {
+              console.error('Error uploading file or creating script:', error);
+            }
+
+          }
+          setSubmitting(false);
         }}
         validationSchema={ScriptSchema}
       >
-        {({ handleSubmit, handleBlur, setFieldValue, values, errors, touched }) => (
+        {({ handleSubmit, handleBlur, setFieldValue,  errors, touched, isSubmitting }) => (
           <Form onSubmit={handleSubmit} className={styles.form}>
             <InfoBlockLayout
               headerClassname={styles.infoblock__header}
@@ -34,21 +63,21 @@ export default function CreatePage() {
             >
               <div className={styles.flex}>
                 <FastField name={`name`}>
-                  {({ field}: any) => (
+                  {({ field }: FastFieldProps) => (
                     <InputLayout
                       type='text'
-                      value={field.value ?? ''}
+                      // value={field.value ?? ''}
                       inputTitle={pageCreateUsecase.main.blocks.scriptTitle.title}
                       className={styles.input}
                       placeholder={pageCreateUsecase.main.blocks.scriptTitle.placeholder}
                       errorText={errors.name && touched.name ? errors.name : null}
                       {...field}
-                      
+
                     />
                   )}
                 </FastField>
                 <FastField name={`file`}>
-                  {({ field }: any) => (
+                  {({ field }: FastFieldProps) => (
                     <InputLayout
                       type='file'
                       name={field.name}
@@ -66,14 +95,14 @@ export default function CreatePage() {
 
               </div>
               <FastField name={`desc`}>
-                {({ field }: any) => (
+                {({ field }: FastFieldProps) => (
                   <InputLayout
                     type='text'
                     inputTitle={pageCreateUsecase.main.blocks.scriptDesc.title}
                     className={styles.input}
                     isTextArea
                     inputClassName={styles.desc}
-                    value={field.value ?? ''}
+                    // value={field.value ?? ''}
                     placeholder={pageCreateUsecase.main.blocks.scriptDesc.placeholder}
                     errorText={errors.desc && touched.desc ? errors.desc : null}
                     {...field}
@@ -84,7 +113,7 @@ export default function CreatePage() {
             </InfoBlockLayout>
             <ScriptParametrsLoader type='input' />
             <ScriptParametrsLoader type='output' />
-            <Button className={styles.saveBtn} icon={<SaveScriptIcon />} type='submit'>Сохранить скрипт</Button>
+            <Button isLoading={isSubmitting} className={styles.saveBtn} icon={<SaveScriptIcon />} type='submit'>Сохранить скрипт</Button>
           </Form>
         )}
       </Formik>
