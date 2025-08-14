764bef5b15d99ee62b780f0147b993fa0935d58e
Robocotik <alexander.starkin@gmail.com>
2025-08-14 18:15:29 +0300

    [feat] components/RunCodeButton: delete business logic

--- src/shared/RunCodeButton/RunCodeButton.props.tsx
+++ src/shared/RunCodeButton/RunCodeButton.props.tsx
@@ -1,5 +1,5 @@
 import type {HTMLAttributes} from 'react';
 
 export interface Props extends HTMLAttributes<HTMLButtonElement> {
-  scriptId: number;
+  isLoading?: boolean;
 }
--- src/shared/RunCodeButton/index.tsx
+++ src/shared/RunCodeButton/index.tsx
@@ -1,11 +1,13 @@
-import {RunIcon} from '@/components/icons/RunIcon';
-import {Button} from '@/shared/Button';
-import {Props} from './RunCodeButton.props';
-import type {FC} from 'react';
+'use client';
+import { RunIcon } from '@/components/icons/RunIcon';
+import { Button } from '@/shared/Button';
+import { Props } from './RunCodeButton.props';
+import type { FC } from 'react';
 
-export const RunCodeButton: FC<Props> = ({scriptId, className, ...props}) => {
+
+export const RunCodeButton: FC<Props> = ({ isLoading = false, className, ...props }) => {
   return (
-    <Button icon={<RunIcon />} className={className} {...props}>
+    <Button type='submit' icon={<RunIcon />} isLoading={isLoading} className={className} {...props} >
       Запустить
     </Button>
   );
