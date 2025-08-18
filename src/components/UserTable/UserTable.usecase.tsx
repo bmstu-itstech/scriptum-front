322d7874c26c322a17c3c35cf568357baef81601
Robocotik <alexander.starkin@gmail.com>
2025-08-18 17:45:03 +0300

    [fix] userTable: fix white color in input

---
+++ src/components/UserTable/UserTable.usecase.tsx
@@ -0,0 +1,27 @@
+import * as Yup from 'yup'
+
+
+
+export const UserValidationSchema = Yup.object().shape({
+	fullname: Yup.string().required('Полное имя обязательно'),
+	email: Yup.string().email('Некорректный email').required('Email обязателен'),
+	password: Yup.string().min(6, 'Пароль должен содержать минимум 6 символов'),
+	confirmPassword: Yup.string().oneOf([Yup.ref('password')], 'Пароли не совпадают'),
+	role: Yup.string().oneOf(['user', 'admin'], 'Некорректная роль')
+})
+
+export const UserTableValidationSchema = Yup.object().shape({
+    users: Yup.array().of(UserValidationSchema)
+})
+
+export const UserInitialValues = {
+    fullname: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    role: 'user'
+}
+
+export const UserTableInitialValues = {
+    users: [],
+}
