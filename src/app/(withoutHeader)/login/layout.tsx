687bd1dd95fcfd119e9435b2fb5cf616fd3431df
Robocotik <alexander.starkin@gmail.com>
2025-09-18 11:35:01 +0300

    [feat] delete bug on auth

--- src/app/(withoutHeader)/login/layout.tsx
+++ src/app/(withoutHeader)/login/layout.tsx
@@ -1,6 +1,21 @@
 // import type {Metadata} from 'next';
-// import {Geist, Geist_Mono, Inter} from 'next/font/google';
-import styles from '@/app/(withoutHeader)/login/page.module.css';
+import { Geist, Geist_Mono, Inter } from 'next/font/google';
+// import styles from '@/app/(withoutHeader)/login/page.module.css';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+const interSans = Inter({
+  variable: '--font-inter',
+  subsets: ['latin'],
+});
 
 export default function LoginLayout({
   children,
@@ -9,7 +24,7 @@ export default function LoginLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={`${styles.body}`}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable} ${interSans.variable}`}>{children}</body>
     </html>
   );
 }
